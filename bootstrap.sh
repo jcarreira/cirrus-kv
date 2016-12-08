474231bb8880ddca6c8b07d8dea93bae961ba9d8
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2016-12-08 14:07:19 -0800

    Added boostrap script

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+# get third party libs
+git submodule init 
+git submodule update
+
+# compile libcuckoo
+cd third_party/libcuckoo
+autoreconf -fis
+./configure
+make -j 10
+
+# compile sparsehash
+cd ../sparsehash
+./configure
+make -j 10
+cd ../..
+
+# main compilation
+autoreconf
+automake --add-missing
+./configure
