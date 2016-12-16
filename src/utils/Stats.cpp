54f186568aba518e34c1961d84952b8f92e6df6b
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2016-12-15 20:32:22 -0800

    Added Stats capability

---
+++ src/utils/Stats.cpp
@@ -0,0 +1,49 @@
+#include "src/utils/Stats.h"
+
+#include <cmath>
+#include <algorithm>
+#include <stdexcept>
+#include <cstdint>
+#include <cassert>
+
+namespace sirius {
+
+Stats::Stats() {
+
+}
+
+void Stats::add(double d){
+    sum += d;
+    sq_sum += d * d;
+    count++;
+    data.push_back(d);
+}
+
+double Stats::avg() const {
+    return sum / count;
+}
+
+double Stats::size() const {
+    return count;
+}
+
+double Stats::sd() const {
+    return sqrt(sq_sum / count - avg() * avg());
+}
+
+double Stats::total() const {
+    return sum;
+}
+
+double Stats::getPercentile(double p) const {
+    if(data.size() == 0)
+        throw std::runtime_error("No data");
+
+    std::sort(data.begin(), data.end());
+    uint32_t loc = static_cast<int>(p * data.size());
+    assert(loc < data.size());
+    return data[loc];
+}
+
+} // namespace sirius
+
---
+++ src/utils/Stats.h
@@ -0,0 +1,29 @@
+#ifndef _STATS_H_
+#define _STATS_H_
+
+#include <vector>
+
+namespace sirius {
+
+class Stats {
+    Stats();
+
+    void add(double d);
+
+    double avg() const;
+    double size() const;
+    double sd() const;
+    double getPercentile(double p) const;
+    double total() const;
+private:
+    mutable std::vector<double> data;
+
+
+    double sum = 0;
+    double sq_sum = 0;
+    double count = 0;
+};
+
+}
+
+#endif // _STATS_H_
