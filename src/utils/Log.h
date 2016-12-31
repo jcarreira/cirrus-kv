1760674b7f96fbaaab3eccf11d2b97bd58aec0c9
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2016-12-30 20:42:08 -0800

    Added prefixes to Log class

--- src/utils/Log.h
+++ src/utils/Log.h
@@ -12,32 +12,44 @@ namespace sirius {
 
 struct INFO {
     static const int value = 5;
+    constexpr static const char* prefix = "[INFO]";
 };
 struct ERROR {
     static const int value = 10;
+    constexpr static const char* prefix = "[ERROR]";
 };
+
+static const int THRESHOLD = 5;
 
 #if __GNUC__ >= 7
-struct FLUSH {
-};
-
-struct NO_FLUSH {
-};
+struct FLUSH { };
+struct NO_FLUSH { };
+struct TIME { };
+struct NO_TIME { };
     
-template<typename T, typename K = NO_FLUSH, typename ... Params>
+template<typename T, typename K = NO_FLUSH, typename TT = TIME, typename ... Params>
 #else
 template<typename T, typename ... Params>
 #endif
 bool LOG(Params&& ... param) {
-//    static SpinLock sl;
-//    sl.wait();
+
+    if (T::value < THRESHOLD)
+        return true;
+
+    std::cout << T::prefix
+        << " -";
+
+#if __GNUC__ >= 7
+    if constexpr (std::is_same(TT, TIME)) {
+        std::cout << getTimeNow();
+    }
+#else
+    std::cout << getTimeNow();
+#endif
 
     auto f = [](const auto& arg) {
         std::cout << " " << arg;
     };
-
-    // print timestamp
-    std::cout << getTimeNow();
 
  __attribute__((unused))
     int dummy[] = { 0, ( (void) f(std::forward<Params>(param)), 0) ... }; 
