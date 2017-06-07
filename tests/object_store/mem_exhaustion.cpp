36d16fb5bc6b9526d51b5c8cd90477880b502ffb
TylerADavis <tyleradavis@me.com>
2017-06-07 14:23:07 -0700

    Comments and removal of unnecessary include

--- tests/object_store/mem_exhaustion.cpp
+++ tests/object_store/mem_exhaustion.cpp
@@ -9,7 +9,7 @@
 static const uint64_t GB = (1024*1024*1024);
 const char PORT[] = "12345";
 const char IP[] = "10.10.49.83";
-static const uint32_t SIZE = 1024*1024;
+static const uint32_t SIZE = 1024*1024; // One MB
 static const uint64_t MILLION = 1000000;
 
 struct Dummy {
@@ -17,14 +17,19 @@ struct Dummy {
     int id;
 };
 
-
+/** This test aims to ensure that when the remote server no longer has room to fulfill
+  * all allocations it notifies the client, which will then throw an error message. 
+  * This test assumes that the server does not have enough room to store one million
+  * objects of one MB each (1 TB). This test also ensures that the allocation error does
+  * not crash the server as in order for notification of failure to reach the client,
+  * the server must have been running to send the message.
+  */
 void test_exhaustion() {
     cirrus::ostore::FullBladeObjectStoreTempl<Dummy> store(IP, PORT);
 
     std::unique_ptr<Dummy> d = std::make_unique<Dummy>();
     d->id = 42;
-    
-    	
+
    // warm up
    std::cout << "Putting 1000" << std::endl;
     for (int i = 0; i < 1000; ++i) {
@@ -37,7 +42,7 @@ void test_exhaustion() {
 
     std::cout << "Putting one million objects" << std::endl;
     for (uint64_t i = 0; i < MILLION; ++i) {
-       	 store.put(d.get(), sizeof(Dummy), i, &mem);
+        store.put(d.get(), sizeof(Dummy), i, &mem);
     }
 }
 
