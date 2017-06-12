b8d19d8d542301017e6781e371fcb3efbd92fa78
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-12 11:49:42 -0700

    migrated mpi test, modified its run script

--- tests/mpi/run.sh
+++ tests/mpi/run.sh
@@ -1,5 +1,4 @@
 #export LD_LIBRARY_PATH=~/
-export LD_LIBRARY_PATH=/data/joao/ddc/third_party/libcuckoo/cityhash-1.1.1/src/.libs
 cp test_mpi ~/
-mpirun -x $LD_LIBRARY_PATH -np 10 ~/test_mpi
+mpirun -np 10 ~/test_mpi
 #mpirun -x $LD_LIBRARY_PATH -hostfile ~/hosts2 -np 1 ~/test_mpi
