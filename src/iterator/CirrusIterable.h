90d5439a49c03061c8b173057df3308902adec60
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-14 12:49:13 -0700

    tenative version of the iterator

--- src/iterator/CirrusIterable.h
+++ src/iterator/CirrusIterable.h
@@ -16,19 +16,22 @@ using ObjectID = uint64_t;
 template<class T>
 class CirrusIterable {
  public:
-    cirrus::Iterator<T> cirrus::CirrusIterable::begin();
-    cirrus::Iterator<T> cirrus::CirrusIterable::end();
+    cirrus::Iterator<T> begin();
+    cirrus::Iterator<T> end();
 
     /**
       * Constructor for the CirrusIterable class. Assumes that all objects
       * are stored sequentially between first and last.
       * @param cm a pointer to a CacheManager with that contains the same
       * object type as this Iterable.
-      * @param first the first sequential objectID.
-      * @param the last sequential id under which an object is stored.
+      * @param first the first sequential objectID. Should always be <= than
+      * last.
+      * @param the last sequential id under which an object is stored. Should
+      * always be >= first.
       * @param readAhead how many items ahead items should be prefetched.
+      * Should always be <= last - first.
       */
-    cirrus::CirrusIterable<T>(cirrus::CacheManager<T>* cm,
+    CirrusIterable<T>(cirrus::CacheManager<T>* cm,
                                  unsigned int readAhead,
                                  ObjectID first,
                                  ObjectID last):
@@ -42,14 +45,20 @@ class CirrusIterable {
     ObjectID last;
 };
 
+/**
+  * Function that returns a cirrus::Iterator at the start of the given range.
+  */
 cirrus::Iterator<T> CirrusIterable<T>::begin() {
-
+    return new Iterator(cm, readAhead, first, last, first);
 }
 
+/**
+  * Function that returns a cirrus::Iterator one past the end of the given
+  * range.
+  */
 cirrus::Iterator<T> CirrusIterable<T>::end() {
-
+  return new Iterator(cm, readAhead, first, last, last + 1);
 }
-
 
 }  // namespace cirrus
 
--- src/iterator/Iterator.h
+++ src/iterator/Iterator.h
@@ -13,13 +13,72 @@ using ObjectID = uint64_t;
   * prefetch the next object
   */
 template<class T>
-class CacheManager {
+class Iterator {
  public:
+    T operator*();
+    Iterator& operator++();
+    Iterator& operator++(int i);
+    bool operator!=(const Iterator& it) const;
+    bool operator==(const Iterator& it) const;
+    ObjectID get_curr_id() const;
+
+   /**
+     * Constructor for the Iterator class. Assumes that all objects
+     * are stored sequentially.
+     * @param cm a pointer to a CacheManager with that contains the same
+     * object type as this Iterable.
+     * @param current_id the id that will be fetched when the iterator is
+     * dereferenced.
+     * @param readAhead how many items ahead items should be prefetched.
+     */
+    Iterator<T>(cirrus::CacheManager<T>* cm,
+                                unsigned int readAhead, ObjectID first,
+                                ObjectID last, ObjectID current_id):
+                                cm(cm), readAhead(readAhead), first(first),
+                                last(last), current_id(current_id) {}
 
  private:
-
+    cirrus::CacheManager<T> *cm;
+    unsigned int readAhead;
+    ObjectID first;
+    ObjectID last;
+    ObjectID current_id;
 };
 
+T Iterator<T>::operator*() {
+  for (unsigned int i = 1, i <= readAhead, i++) {
+    // Math to make sure that prefetching loops back around
+    // Formula is val = ((current_id + i) - first) % (last - first)) + first
+    ObjectID tenative_fetch = current_id + i;  // calculate what we WOULD fetch
+    ObjectID shifted = tenative_fetch - first;  // shift relative to first
+    ObjectID modded = shifted % (last - first);  // Mod relative to shifted last
+    ObjectID to_fetch = modded + first;  // Add back to first for final result
+    cm->prefetch(to_fetch);
+  }
+
+  return cm->get(current_id);
+}
+
+Iterator<T>& Iterator<T>::operator++() {
+  current_id++;
+  return *this;
+}
+
+Iterator<T>& Iterator<T>::operator++(int /* i */) {
+  current_id++;
+  return *this;
+}
+
+bool Iterator<T>::operator!=(const Iterator<T>& it) const {
+  return current_id != it.get_curr_id();
+}
+
+bool Iterator<T>::operator==(const Iterator<T>& it) const {
+  return current_id == it.get_curr_id();
+}
+
+int Iterator<T>::get_curr_id() const {
+  return current_id;
 
 }  // namespace cirrus
 
