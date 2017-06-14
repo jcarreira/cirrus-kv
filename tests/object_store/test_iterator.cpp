01094949860eaa144156c313c69edbdb3b880974
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-14 13:01:45 -0700

    wrote test for iterator

---
+++ tests/object_store/test_iterator.cpp
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <iterator>
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <memory>
+
+#include "src/object_store/FullBladeObjectStore.h"
+#include "src/object_store/object_store_internal.h"
+#include "src/cache_manager/CacheManager.h"
+#include "src/iterator/Iterator.h"
+#include "src/iterator/CirrusIterable.h"
+
+
+static const uint64_t GB = (1024*1024*1024);
+const char PORT[] = "12345";
+unsigned int SIZE = 1;
+const char IP[] = "10.10.49.83";
+
+
+// #define CHECK_RESULTS
+void test_iterator() {
+  cirrus::ostore::FullBladeObjectStoreTempl<cirrus::Dummy<SIZE>> store(IP, PORT,
+                      cirrus::struct_serializer_simple<SIZE>,
+                      cirrus::struct_deserializer_simple<SIZE>);
+
+  cirrus::CacheManager<cirrus::DUMMY<SIZE>> cm(&store, 10);
+
+
+    // Put items in the store
+    for (int i = 0; i < 10; i++) {
+      try {
+          cirrus::Dummy<SIZE> d(i);
+          cm.put(i, d);
+      } catch(...) {
+          std::cerr << "Error inserting" << std::endl;
+      }
+    }
+
+    // Use iterator to retrieve
+    cirrus::CirrusIterable iter(&cm, 1, 0, 9);
+
+    int j = 0;
+    for (auto it = iter.begin(); it != iter.end(); it++) {
+      cirrus::Dummy<SIZE> val = *it;
+      if (val.id != j) {
+        printf("received %d but expected %d\n", val.id, j);
+        throw std::runtime_error("Wrong value");
+      }
+      j++;
+    }
+}
+
+auto main() -> int {
+    test_iterator();
+    return 0;
+}
