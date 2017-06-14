18004832cc0dca9ffeccce9c1cf2b3f523ca2333
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-14 16:57:26 -0700

    test updated and added to make check

--- tests/object_store/test_iterator.cpp
+++ tests/object_store/test_iterator.cpp
@@ -54,7 +54,40 @@ void test_iterator() {
     }
 }
 
+void test_iterator_alt() {
+  cirrus::ostore::FullBladeObjectStoreTempl<cirrus::Dummy<SIZE>> store(IP, PORT,
+                      cirrus::struct_serializer_simple<SIZE>,
+                      cirrus::struct_deserializer_simple<SIZE>);
+
+  cirrus::CacheManager<cirrus::Dummy<SIZE>> cm(&store, 10);
+
+
+    // Put items in the store
+    for (int i = 0; i < 10; i++) {
+      try {
+          cirrus::Dummy<SIZE> d(i);
+          cm.put(i, d);
+      } catch(...) {
+          std::cerr << "Error inserting" << std::endl;
+      }
+    }
+
+    // Use iterator to retrieve
+    cirrus::CirrusIterable<cirrus::Dummy<SIZE>> iter(&cm, 1, 0, 9);
+
+    int j = 0;
+    for (const auto& data : iter) {
+      cirrus::Dummy<SIZE> val = *data;
+      if (val.id != j) {
+        printf("received %d but expected %d\n", val.id, j);
+        throw std::runtime_error("Wrong value");
+      }
+      j++;
+    }
+}
+
 auto main() -> int {
     test_iterator();
+    test_iterator_alt();
     return 0;
 }
