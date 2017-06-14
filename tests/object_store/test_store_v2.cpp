222892d8feee301cfe8c083ea5ae2115ceb21fe1
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-14 15:42:44 -0700

    added serializer_simple and deserializer to the common file

--- tests/object_store/test_store_v2.cpp
+++ tests/object_store/test_store_v2.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "src/object_store/FullBladeObjectStore.h"
+#include "src/object_store/object_store_internal.h"
 #include "src/utils/Time.h"
 #include "src/utils/Stats.h"
 
@@ -10,31 +11,14 @@ static const uint64_t GB = (1024*1024*1024);
 const char PORT[] = "12345";
 const char IP[] = "10.10.49.83";
 
-
-/* This function simply copies an int into a new portion of memory. */
-std::pair<std::unique_ptr<char[]>, unsigned int>
-    serializer_simple(const int& v) {
-
-    std::unique_ptr<char[]> ptr(new char[sizeof(int)]);
-    *(ptr.get()) = v;
-    return std::make_pair(std::move(ptr), sizeof(int));
-}
-
-/* Takes an int passed in and returns as object. */
-int deserializer_simple(void* data, unsigned int /* size */) {
-    int *ptr = reinterpret_cast<int *>(data);
-    int retval = *ptr;
-    return retval;
-}
-
-
 /**
   * Test simple synchronous put and get to/from the object store.
   * Uses simpler objects than test_fullblade_store.
   */
 void test_store_simple() {
     cirrus::ostore::FullBladeObjectStoreTempl<int> store(IP, PORT,
-                        serializer_simple, deserializer_simple);
+                        cirrus::serializer_simple,
+                        cirrus::deserializer_simple);
     for (int oid = 0; oid <  10; oid++) {
       store.put(oid, oid);
     }
