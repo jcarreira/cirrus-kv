e05473a1895d0cfc699866ce533cbbd99681a573
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-15 10:48:52 -0700

    adding new cache manager tests

--- tests/object_store/test_cache_manager.cpp
+++ tests/object_store/test_cache_manager.cpp
@@ -12,21 +12,6 @@ static const uint64_t GB = (1024*1024*1024);
 const char PORT[] = "12345";
 const char IP[] = "10.10.49.83";
 
-/* This function simply copies an int into a new portion of memory. */
-std::pair<std::unique_ptr<char[]>, unsigned int>
-    serializer_simple(const int& v) {
-    std::unique_ptr<char[]> ptr(new char[sizeof(int)]);
-    std::memcpy(ptr.get(), &v, sizeof(int));
-    return std::make_pair(std::move(ptr), sizeof(int));
-}
-
-/* Takes an int passed in and returns as object. */
-int deserializer_simple(void* data, unsigned int /* size */) {
-    int *ptr = reinterpret_cast<int *>(data);
-    int retval = *ptr;
-    return retval;
-}
-
 /**
   * Test simple synchronous put and get to/from the object store.
   * Uses simpler objects than test_fullblade_store.
@@ -49,8 +34,55 @@ void test_cache_manager_simple() {
     }
 }
 
+void test_nonexistent_get() {
+    cirrus::ostore::FullBladeObjectStoreTempl<int> store(IP, PORT,
+                        cirrus::serializer_simple,
+                        cirrus::deserializer_simple);
+
+    cirrus::CacheManager<int> cm(&store, 10);
+    for (int oid = 0; oid <  10; oid++) {
+        cm.put(oid, oid);
+    }
+
+    // Should fail
+    cm.get(10);
+}
+
+void test_capacity() {
+    cirrus::ostore::FullBladeObjectStoreTempl<int> store(IP, PORT,
+                        cirrus::serializer_simple,
+                        cirrus::deserializer_simple);
+
+    cirrus::CacheManager<int> cm(&store, 10);
+    for (int oid = 0; oid <  15; oid++) {
+        cm.put(oid, oid);
+    }
+
+    for (int oid = 0; oid < 10; oid++) {
+        cm.get(oid);
+    }
+
+    // Should fail
+    cm.get(10);
+}
+
 auto main() -> int {
     std::cout << "test starting" << std::endl;
     test_cache_manager_simple();
+    try {
+        test_capacity();
+        std::cout << "Exception not thrown when cache"
+                     " capacity exceeded." << std::endl;
+        return -1;
+    } catch (const cirrus::CacheFilledException & e) {
+    }
+    try {
+        test_nonexistent_get();
+        std::cout << "Exception not thrown when get"
+                     " called on nonexistent ID." << std::endl;
+        return -1;
+    } catch (const cirrus::NoSuchIDException & e) {
+    }
+
     return 0;
 }
