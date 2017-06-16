9ee99fff9e69ed84b862f1dd94b1ac6472cb9b09
Tyler Davis <tylerdavis@berkeley.edu>
2017-06-16 10:52:42 -0700

    simplified all tests, renamed some

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,22 +3,9 @@
 import sys
 import subprocess
 import time
+import test_runner
 
-# NOTE: all pathnames start from the top directory where make check is run
-
-# Launch the server in the background
-print("Starting server.")
-server = subprocess.Popen(["./src/server/bladeallocmain"])
-
-# Sleep to give server time to start
-print("Started server, sleeping.")
-time.sleep(2)
-print("Sleep finished, launching client.")
-
-child = subprocess.Popen(["./src/client/clientmain"], stdout=subprocess.PIPE)
-streamdata = child.communicate()[0]
-rc = child.returncode
-
-server.kill()
-
-sys.exit(rc)
+# Set name of test to run
+testPath = "./src/server/bladeallocmain"
+# Call script to run the test
+test_runner.runTest(testPath)
--- tests/test_mem_exhaustion.py
+++ tests/test_mem_exhaustion.py
@@ -3,24 +3,9 @@
 import sys
 import subprocess
 import time
+import test_runner
 
-# NOTE: all pathnames start from the top directory where make check is run
-
-# Launch the server in the background
-print("Starting server.")
-# Sleep to give the server from the previous test time to close
-time.sleep(1)
-server = subprocess.Popen(["./src/server/bladeallocmain"])
-
-# Sleep to give server time to start
-print("Started server, sleeping.")
-time.sleep(2)
-print("Sleep finished, launching client.")
-
-child = subprocess.Popen(["./tests/object_store/exhaustion"], stdout=subprocess.PIPE)
-streamdata = child.communicate()[0]
-rc = child.returncode
-
-server.kill()
-
-sys.exit(rc)
+# Set name of test to run
+testPath = "./tests/object_store/exhaustion"
+# Call script to run the test
+test_runner.runTest(testPath)
