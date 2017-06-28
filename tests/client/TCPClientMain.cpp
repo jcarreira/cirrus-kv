f221d58b2153bc0750e400f21b450a74733f9a8f
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2017-06-27 22:56:53 -0700

    Changed IP to localhost. Fixed alignment

--- tests/client/TCPClientMain.cpp
+++ tests/client/TCPClientMain.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 #include "client/TCPClient.h"
 
-
 // TODO: Remove hardcoded port and ip
 auto main() -> int {
     std::string port = "12345";
-    std::string ip = "10.10.49.83";
+    std::string ip = "127.0.0.1";
     cirrus::TCPClient client;
+
     client.connect(ip, port);
+
     int message = 42;
     client.write_sync(1, &message, sizeof(int));
     std::cout << "write sync complete" << std::endl;
+
     int returned;
     client.read_sync(1, &returned, sizeof(int));
     std::cout << returned << " returned from server" << std::endl;
