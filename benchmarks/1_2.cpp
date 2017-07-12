09b6f3ae90c6ba8182392db6ddc737b0e90ccf8c
TylerADavis <tyleradavis@me.com>
2017-07-11 17:22:31 -0700

    Changed so deserializer copies full object (#78)

--- benchmarks/1_2.cpp
+++ benchmarks/1_2.cpp
@@ -34,7 +34,8 @@ void test_async(int N) {
     cirrus::ostore::FullBladeObjectStoreTempl<cirrus::Dummy<SIZE>>
         store(IP, PORT, &client,
             cirrus::serializer_simple<cirrus::Dummy<SIZE>>,
-            cirrus::deserializer_simple<cirrus::Dummy<SIZE>, SIZE>);
+            cirrus::deserializer_simple<cirrus::Dummy<SIZE>,
+                sizeof(cirrus::Dummy<SIZE>)>);
     cirrus::Stats stats;
 
     struct cirrus::Dummy<SIZE> d(42);
