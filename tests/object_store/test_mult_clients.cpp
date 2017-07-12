43a418a11c9b10667258542b0d6bedf24313ff44
Tyler Davis <tylerdavis@berkeley.edu>
2017-07-12 10:56:49 -0700

    Test mult clients passing

--- tests/object_store/test_mult_clients.cpp
+++ tests/object_store/test_mult_clients.cpp
@@ -9,6 +9,7 @@
 
 #include "object_store/FullBladeObjectStore.h"
 #include "tests/object_store/object_store_internal.h"
+#include "common/Synchronization.h"
 #include "utils/Time.h"
 #include "client/TCPClient.h"
 
@@ -21,11 +22,12 @@ const char IP[] = "127.0.0.1";
 static const uint32_t SIZE = 1024;
 
 uint64_t total_puts = 0;
+cirrus::SpinLock total_puts_lock;
 
 /**
-  * Checks that the system works properly when multiple clients get and put.
-  * These clients each use their own instance of the store.
-  */
+ * Checks that the system works properly when multiple clients get and put.
+ * These clients each use their own local store and client instances.
+ */
 void test_multiple_clients() {
     cirrus::TimerFunction tf("connect time", true);
 
@@ -34,33 +36,35 @@ void test_multiple_clients() {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 10);
-
+    int start = 0;
+    int stop = 10;
     for (int i = 0; i < N_THREADS; ++i) {
-        threads[i] = new std::thread([dis, gen]() {
-          cirrus::TCPClient client;
-          cirrus::ostore::FullBladeObjectStoreTempl<cirrus::Dummy<SIZE>>
-              store(IP, PORT, &client,
-                       cirrus::serializer_simple<cirrus::Dummy<SIZE>>,
-                       cirrus::deserializer_simple<cirrus::Dummy<SIZE>,
-                           sizeof(cirrus::Dummy<SIZE>)>);
-
-          for (int i = 0; i < 100; ++i) {
+        threads[i] = new std::thread([dis, gen, start, stop]() {
+            cirrus::TCPClient client;
+            cirrus::ostore::FullBladeObjectStoreTempl<cirrus::Dummy<SIZE>>
+                store(IP, PORT, &client,
+                    cirrus::serializer_simple<cirrus::Dummy<SIZE>>,
+                    cirrus::deserializer_simple<cirrus::Dummy<SIZE>,
+                        sizeof(cirrus::Dummy<SIZE>)>);
+            for (int i = start; i < stop; i++) {
                 int rnd = std::rand();
                 struct cirrus::Dummy<SIZE> d(rnd);
+                store.put(i, d);
 
-                store.put(1, d);
-
-                struct cirrus::Dummy<SIZE> d2 = store.get(1);
+                struct cirrus::Dummy<SIZE> d2 = store.get(i);
 
                 if (d2.id != rnd) {
                     std::cout << "Expected " << rnd << " but got " << d2.id
                         << std::endl;
                     throw std::runtime_error("Wrong value returned.");
                 }
-
+                total_puts_lock.wait();
                 total_puts++;
+                total_puts_lock.signal();
             }
         });
+        start += 10;
+        stop += 10;
     }
 
     for (int i = 0; i < N_THREADS; ++i)
