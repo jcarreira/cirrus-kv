7280f2cf3dfc5e20d8f201635abf8930d6a3ab1b
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2017-08-07 01:09:17 -0700

    Couple of fixes

--- tests/ml/run.sh
+++ tests/ml/run.sh
@@ -1,1 +1,2 @@
-mpirun -hostfile ~/hosts -np 4 ~/parameter_server args
+mpirun -hostfile ~/hosts -np 4 gdb -ex run -ex bt --args ~/parameter_server args
+# -mpirun -hostfile ~/hosts -np 4 ~/parameter_server args
