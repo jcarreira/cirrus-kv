3aab1f87afa6efc3ed62cf07cca35cfbe26a8ac3
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2017-08-17 19:20:02 -0700

    Refactoring

--- tests/ml/run.sh
+++ tests/ml/run.sh
@@ -8,4 +8,4 @@ cp parameter_server ~/parameter_server
 #mpirun -hostfile ~/hosts -np 5 valgrind --log-file=valgrind_output --suppressions=/home/eecs/joao/vg_suppressions_cirrus --error-limit=no ~/parameter_server ~/criteo_fbox.cfg
 
 # plain
-mpirun -output-filename ~/ps_output -hostfile ~/hosts -np 5 ~/parameter_server ~/criteo_fbox.cfg
+mpirun -output-filename ~/ps_output -hostfile ~/hosts -np 6 ~/parameter_server ~/criteo_fbox.cfg
