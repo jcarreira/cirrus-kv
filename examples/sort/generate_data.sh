65e19b804ed322274a2947d098148231d57eb0ee
jcarreira <joao@berkeley.edu>
2017-09-13 12:19:44 -0700

    Update

--- examples/sort/generate_data.sh
+++ examples/sort/generate_data.sh
@@ -3,7 +3,7 @@
 # use code from https://github.com/ehiggs/spark-terasort
 
 PATH_TO_SPARK_TERASORT=/data/joao/spark-terasort
-OUTPUT_PATH=/scratch/joao/terasort_in
+OUTPUT_PATH=/data/joao/cirrus/examples/sort/terasort_in
 
 /usr/local/spark/bin/spark-submit --class com.github.ehiggs.spark.terasort.TeraGen \
      ${PATH_TO_SPARK_TERASORT}/target/spark-terasort-1.0-jar-with-dependencies.jar \
